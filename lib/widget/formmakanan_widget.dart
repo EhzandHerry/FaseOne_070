d342e4a390da4060808f3f08a92af76080fe9789
21PAML070 <ehzand.herry1926@gmail.com>
2024-03-15 20:54:37 +0700

    membuat file widget untuk form makanan menggunakan statefull

---
+++ lib/widget/formmakanan_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormMakananWidget extends StatefulWidget {
+  const FormMakananWidget({super.key});
+
+  @override
+  State<FormMakananWidget> createState() => _FormMakananWidgetState();
+}
+
+class _FormMakananWidgetState extends State<FormMakananWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
