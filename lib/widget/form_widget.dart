63b31e221078994982faa1784c62e4fa436686e2
21PAML070 <ehzand.herry1926@gmail.com>
2024-03-15 20:00:10 +0700

    menambahkan radio button untuk jenis kelamin.

--- lib/widget/form_widget.dart
+++ lib/widget/form_widget.dart
@@ -18,63 +18,96 @@ class FormWidget extends StatefulWidget {
 }
 
 class _FormWidgetState extends State<FormWidget> {
+  int selectedOption = 1;
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: widget.formKey,
-        child: Column(
-          children: [
-            TextFormField(
-              controller: widget.etNama,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "Nama",
-                hintText: "Masukkan Nama",
-                prefixIcon: Icon(Icons.person),
+      key: widget.formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            controller: widget.etNama,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: "Nama",
+              hintText: "Masukkan Nama",
+              prefixIcon: Icon(Icons.person),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Masukkan nama terlebih dahulu!';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: widget.etAlamat,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+                labelText: "Alamat",
+                hintText: "Masukkan Alamat",
+                prefixIcon: Icon(Icons.location_city)),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Masukkan alamat terlebih dahulu!';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            controller: widget.etNotelp,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "No Telepon",
+              hintText: "Masukkan No Telp",
+              prefixIcon: Icon(Icons.phone),
+            ),
+            validator: (value) {
+              bool isValidPhoneNumber(String? value) => RegExp(
+                      r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)')
+                  .hasMatch(value!);
+              if (value!.isEmpty) {
+                return "Masukkan notelp dengan benar";
+              }
+              return null;
+            },
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Jenis Kelamin", style: TextStyle(fontSize: 20),),
+              Row(
+                children: [
+                  Radio<int>(
+                    value: 1,
+                    groupValue: selectedOption,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedOption = value!;
+                      });
+                    },
+                  ),
+                  Text('P'),
+                  SizedBox(width: 20), 
+                  Radio<int>(
+                    value: 2,
+                    groupValue: selectedOption,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedOption = value!;
+                      });
+                    },
+                  ),
+                  Text('L'),
+                ],
               ),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Masukkan nama terlebih dahulu!';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: widget.etAlamat,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  labelText: "Alamat",
-                  hintText: "Masukkan Alamat",
-                  prefixIcon: Icon(Icons.location_city)),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Masukkan alamat terlebih dahulu!';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              controller: widget.etNotelp,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "No Telepon",
-                hintText: "Masukkan No Telp",
-                prefixIcon: Icon(Icons.phone),
-              ),
-              validator: (value) {
-                bool isValidPhoneNumber(String? value) => RegExp(
-                        r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)')
-                    .hasMatch(value!);
-                if (value!.isEmpty) {
-                  return "Masukkan notelp dengan benar";
-                }
-                return null;
-              },
-            ),
-          ],
-        ));
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
