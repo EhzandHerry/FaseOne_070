b4b8012a28af20ae907996e97018c65bb237e728
21PAML070 <ehzand.herry1926@gmail.com>
2024-03-15 20:51:47 +0700

    menampilkan notelp pada halaman form makanan

--- lib/screen/dataMakananScreen.dart
+++ lib/screen/dataMakananScreen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class dataMakananScreen extends StatelessWidget {
-  const dataMakananScreen({super.key, required this.nama, required this.notelp});
+  const dataMakananScreen(
+      {super.key, required this.nama, required this.notelp});
 
   final String nama;
   final String notelp;
@@ -10,13 +11,14 @@ class dataMakananScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Data Form"),
+        title: Text("Data Form Makanan"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text("Nama: $nama"),
+            Text("No Telepon: $notelp"),
           ],
         ),
       ),
